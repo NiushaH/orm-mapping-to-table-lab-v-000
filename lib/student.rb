d57aa1ace6de07b693647af3922f2b38d6acbd71
Niusha H <niusha.hadziomerovic@gmail.com>
2019-08-11 23:40:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,7 +13,7 @@ class Student
 
   def self.create_table
     sql =  <<-SQL 
-      CREATE TABLE IF NOT EXISTS songs (
+      CREATE TABLE IF NOT EXISTS student (
         id INTEGER PRIMARY KEY, 
         name TEXT, 
         grade INTEGER
@@ -22,7 +22,19 @@ class Student
     DB[:conn].execute(sql)
   end 
   
+  def self.drop_table
+    sql =  <<-SQL 
+      CREATE TABLE IF NOT EXISTS student (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        grade INTEGER
+        )
+        SQL
+    DB[:conn].execute(sql)
+  end 
   
-  
+  def self.save
+    
+  end 
   
 end
