ca45bf1ac51491c96df7b5d2333ae2e1753a586e
Niusha H <niusha.hadziomerovic@gmail.com>
2019-08-11 23:32:06 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,7 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
-  def initialize(, id=)
+  def initialize(, id=nil)
     @id = id
   end 
   
